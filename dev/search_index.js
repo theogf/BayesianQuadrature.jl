7b2c4d3ab39974f73491c287dc80156929ffc6b3
Documenter.jl <documenter@juliadocs.github.io>
2021-02-08 14:40:11 +0000

    build based on faa3cbe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BayesianQuadrature","category":"page"},{"location":"#BayesianQuadrature","page":"Home","title":"BayesianQuadrature","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BayesianQuadrature]","category":"page"}]
+}
