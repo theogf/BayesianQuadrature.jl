6199ab9c9d4b8cf078266223f1a97d606841ebf6
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2021-02-09 15:06:13 +0100

    Passing tests

--- test/interface.jl
+++ test/interface.jl
@@ -1,14 +1,15 @@
-# @testset "Interface" begin
-rng = Random.MersenneTwister(42)
-D = 5
-prior = MvNormal(ones(D))
-likelihood = MvNormal(0.1 * ones(D))
-integrand = x->pdf(likelihood, x)
-m = BQ.BayesModel(prior, integrand)
-s = BQ.MonteCarlo()
-i = BQ.BMC(transform(SEKernel(), 10.0))
-pZ, x  = BQ.bayesquad(m, i, s; nsamples=100)
-Σ = inv(inv(cov(prior)) + inv(cov(likelihood)))
-posterior = MvNormal(Diagonal(Σ))
-trueZ = 1 / sqrt(det(2π * Σ))
-@test mean(bqZ) ≈ trueZ atol=1e-1
+@testset "Interface" begin
+    rng = Random.MersenneTwister(42)
+    D = 5
+    prior = MvNormal(0.5 * ones(D))
+    likelihood = MvNormal(7.0 * ones(D))
+    integrand = x->pdf(likelihood, x)
+    m = BQ.BayesModel(prior, integrand)
+    s = BQ.MonteCarlo()
+    i = BQ.BMC(transform(SEKernel(), 0.1))
+    pZ, x  = BQ.bayesquad(m, i, s; nsamples=100)
+    Σ = cov(prior) * inv(cov(prior) + cov(likelihood)) * cov(likelihood)
+    posterior = MvNormal(Diagonal(Σ))
+    trueZ = exp(-0.5 * (logdet(cov(prior) + cov(likelihood)) + D * log(2π)))
+    @test mean(pZ) ≈ trueZ atol=1e-3
+end
