6b82c835daa75eeed374ac268cab2a49dc8e2231
Johannes Giersdorf <johannes.giersdorf@gmail.com>
2021-02-17 14:51:34 +0100

    test simplified

--- test/models.jl
+++ test/models.jl
@@ -7,15 +7,8 @@
     m = BayesModel(prior, integrand)  
 
     x1 = randn(rng,Float64,D)
-    x2 = randn(rng,Float64,D)
-    x3 = randn(rng,Float64,D)
-    x4 = randn(rng,Float64,D)
     @test logprior(m)(x1) ≈ logpdf(prior,x1)
-    @test logprior(m)(x2) ≈ logpdf(prior,x2)
-    @test logprior(m)(x3) ≈ logpdf(prior,x3)
-    @test logprior(m)(x4) ≈ logpdf(prior,x4)
     @test logjoint(m)(x1) ≈ logpdf(prior,x1) + logpdf(likelihood,x1)
-    @test logjoint(m)(x2) ≈ logpdf(prior,x2) + logpdf(likelihood,x2)
-    @test logjoint(m)(x3) ≈ logpdf(prior,x3) + logpdf(likelihood,x3)
-    @test logjoint(m)(x4) ≈ logpdf(prior,x4) + logpdf(likelihood,x4)
+    @test BQ.prior(m) == prior
+    @test BQ.integrand(m) == integrand
 end
