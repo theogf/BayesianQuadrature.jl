d5e418d4921b8ae5304c8b80cc3ddaa2b3f8a6c0
Johannes Giersdorf <johannes.giersdorf@gmail.com>
2021-04-10 09:42:16 +0200

    fix negative values due to numerical error #24

--- src/bayesquads/bayesquad.jl
+++ src/bayesquads/bayesquad.jl
@@ -55,7 +55,8 @@ function quadrature(
     K = kernelpdmat(kernel(bquad), samples)
     z = calc_z(samples, prior(model), bquad)
     C = calc_C(prior(model), bquad)
-    return Normal(evaluate_mean(z, K, y), evaluate_var(z, K, C))
+    var = evaluate_var(z, K, C)
+    return Normal(evaluate_mean(z, K, y), max(var,zero(var)))
 end
 
 Λ(bquad::BayesQuad{<:SqExponentialKernel,<:Real}) = abs2(bquad.l) * I
