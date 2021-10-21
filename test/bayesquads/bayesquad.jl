6986e3f45fcbdce22f53169a34f02d2a5b605f78
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-10-21 12:28:03 +0200

    Fixed tests

--- test/bayesquads/bayesquad.jl
+++ test/bayesquads/bayesquad.jl
@@ -3,9 +3,9 @@
     l = [1.0, 2.0]
     L = LowerTriangular(rand(2, 2))
     k = SqExponentialKernel()
-    @test BQ.Λ(BayesQuad(k ∘ ScaleTransform(s))) ≈ 1/s^2 * I
-    @test BQ.Λ(BayesQuad(k ∘ ARDTransform(l))) ≈ Diagonal(inv.(abs2.(l)))
-    @test BQ.Λ(BayesQuad(k ∘ LinearTransform(L))) ≈ inv(L) * inv(L)'
+    @test BQ.Λ(s) ≈ s^2 * I
+    @test BQ.Λ(l) ≈ Diagonal(abs2.(l))
+    @test BQ.Λ(L) ≈ L * L'
 
     σ = 4.0
     @test BQ.scale(BayesQuad(σ * k)) == σ
